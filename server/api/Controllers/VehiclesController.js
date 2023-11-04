e70555cfdafe65290e2f5c8d2b2cf14551ec5a7a
tuani <martinellituani27@gmail.com>
2023-11-04 16:49:39 -0300

    create controller

---
+++ server/api/Controllers/VehiclesController.js
@@ -0,0 +1,121 @@
+import Vehicle from "../Models/Vehicles";
+
+const index = async (req, res) => {
+  try {
+    const vehicle = await Vehicle.findAll();
+    return res.json(vehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao buscar veículo." });
+  }
+};
+
+const findOne = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const vehicle = await Vehicle.findByPk(id);
+
+  if (!vehicle) {
+    return res.status(404).json({ erro: "Veículo não encontrado." });
+  }
+
+  return res.json(vehicle);
+};
+
+const create = async (req, res) => {
+  try {
+    const {
+      rental_company,
+      model,
+      brand,
+      year,
+      engine,
+      doors,
+      shift_model,
+      air_conditioner,
+    } = req.body;
+
+    const newvehicle = await Vehicle.create({
+      rental_company,
+      model,
+      brand,
+      year,
+      engine,
+      doors,
+      shift_model,
+      air_conditioner,
+    });
+
+    res.status(201).json(newvehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao criar veículo." });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const {
+      rental_company,
+      model,
+      brand,
+      year,
+      engine,
+      doors,
+      shift_model,
+      air_conditioner,
+    } = req.body;
+
+    const vehicle = await Vehicle.findByPk(id);
+
+    if (!vehicle) {
+      return res.status(404).json({ erro: "Veículo não encontrado." });
+    }
+
+    await vehicle.update({
+      rental_company,
+      model,
+      brand,
+      year,
+      engine,
+      doors,
+      shift_model,
+      air_conditioner,
+    });
+    return res.json(vehicle);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao atualizar veículo." });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const vehicle = await Vehicle.findByPk(id);
+
+    if (!vehicle) {
+      return res.status(404).json({ erro: "Veículo não encontrado." });
+    }
+
+    await vehicle.destroy();
+    return res.status(200).json("Veículo deletado com sucesso!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Não foi possível deletar veículo." });
+  }
+};
+
+export default {
+    index, 
+    findOne,
+    create,
+    update,
+    remove
+}
---
+++ server/api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import VehiclesController from "./Controllers/VehiclesController";
+
+const app = express();
+app.use(express.json());
+
+app.get('/veiculos', async (req, res) => await VehiclesController.index(req, res));
+
+app.get('/veiculos/:id', async (req, res) => await VehiclesController.findOne(req, res));
+
+app.post('/veiculos', async (req, res) => await VehiclesController.create(req, res));
+
+app.put('/veiculos/:id', async (req, res) => await VehiclesController.update(req, res));
+
+app.delete('/veiculos/:id', async (req, res) => await VehiclesController.remove(req, res));
+
+app.listen(3333, () => {
+    console.log('%cServidor rodando na porta 3333', 'color: green');
+});
+
+export default {
+    path: '/api', 
+    handler: app
+}
